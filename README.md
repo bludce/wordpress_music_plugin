995accc2c04bb1fa6b9e096e6b77851c815cbd9e
Aleksey Zadorozhny <AleksZadorozhny90@gmail.com>
2018-11-29 23:58:56 +0300

    Небольшая подготовка к разработке

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # wordpress_music_plugin
+
+This is a music player in the style of vinyl player.
+This plugin is designed for the site hochzeit-luxury.com
---
+++ vinyl-player.php
@@ -0,0 +1,8 @@
+/*
+Plugin Name: Vinyl-player
+Description: This is a music player in the style of vinyl player
+Version: 1.0
+Author: Aleksey Zadorozhny
+Author URI: https://github.com/bludce/
+Plugin URI: https://github.com/bludce/wordpress_music_plugin
+*/
